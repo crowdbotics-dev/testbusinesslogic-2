cafe87e6752066e75b536ef20151715417cbd1ba
Crowdbotics <engineering@crowdbotics.com>
2021-11-24 16:11:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "testbusinesslogic_2",
+  url: "https://testbusinesslogic_2.botics.co",
+  api: "https://testbusinesslogic_2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
